100e63249019e4c1053404a34784bad7eabc1e8f
charles <codelyfewithcharlie@gmail.com>
2019-11-22 09:19:13 -0700

    updates

--- src/Pages/Home/Home.jsx
+++ src/Pages/Home/Home.jsx
@@ -1,12 +1,20 @@
 import React from "react";
 import style from "./Home.module.scss";
-import Header from "../../components/Header/Header";
+
 const Home = () => {
-	return (
-		<div className={style.home}>
-			<Header />
-			<div>Home Page</div>
-		</div>
-	);
+  return (
+    <div className={style.home}>
+      <div className={style.body}>
+        <div className={style.profile}></div>
+        <h1>Charles Campbell</h1>
+        <h3>Front End Developer</h3>
+        <p>
+          Lorem ipsum dolor sit, amet consectetur adipisicing elit. Ab magni
+          iusto eos temporibus nostrum reprehenderit! Quidem, libero!
+          Doloremque, earum tempore!
+        </p>
+      </div>
+    </div>
+  );
 };
 export default Home;
--- src/index.js
+++ src/index.js
@@ -2,11 +2,5 @@ import React from "react";
 import "./index.scss";
 import ReactDOM from "react-dom";
 import App from "./container/App";
-import { BrowserRouter } from "react-router-dom";
 
-ReactDOM.render(
-	<BrowserRouter>
-		<App />
-	</BrowserRouter>,
-	document.getElementById("root")
-);
+ReactDOM.render(<App />, document.getElementById("root"));
