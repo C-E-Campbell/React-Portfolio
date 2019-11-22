393279557d091660891f2a7f1eafb3f1c9a823e3
charles <codelyfewithcharlie@gmail.com>
2019-11-22 13:16:22 -0700

    updates

--- src/Components/Project/Project.jsx
+++ src/Components/Project/Project.jsx
@@ -5,14 +5,19 @@ export default function Project(props) {
     <div className={style.projectContainer}>
       <img src={props.img} alt="project poster" />
       <h2>{props.title}</h2>
-      <h4>Project Description</h4>
+      <h4>
+        Lorem ipsum dolor, sit amet consectetur adipisicing elit. Consequatur
+        rerum quaerat expedita magnam impedit aliquam.
+      </h4>
       <ul>
-        <li></li>
-        <li></li>
-        <li></li>
-        <li></li>
-        <li></li>
+        <li>Tech 1</li>
+        <li>Tech 2</li>
+        <li>Tech 3</li>
+        <li>Tech 4</li>
+        <li>Tech 5</li>
       </ul>
+      <button className={style.projectBtn}>Live Site</button>
+      <button className={style.projectBtn}>View Code</button>
     </div>
   );
 }
