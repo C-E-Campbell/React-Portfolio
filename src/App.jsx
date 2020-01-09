a4ca10f636c2d30f3ff57f5536c26e08205afd14
charles <codelyfewithcharlie@gmail.com>
2020-01-08 22:16:40 -0500

    updates

--- src/App.jsx
+++ src/App.jsx
@@ -5,6 +5,14 @@ import horror from "./assets/horror.jpg";
 import devtennis from "./assets/devtennis.jpg";
 import group from "./assets/group.jpg";
 import Header from "./Components/Header/Header";
+import Sass from "./assets/sasscomp.png";
+import Redux from "./assets/reduxcomp.png";
+import Reactpng from "./assets/reactcomp.png";
+import Postgresql from "./assets/postgresqlcomp.png";
+import Node from "./assets/nodecomp.png";
+import JS from "./assets/jscomp.jpg";
+import Figma from "./assets/figmacomp.png";
+import Express from "./assets/expresscomp.png";
 const App = () => {
   return (
     <div className={style.home}>
@@ -13,13 +21,36 @@ const App = () => {
         <div className={style.profile}></div>
         <h1>Charles Campbell</h1>
         <h3>Full Stack Web Developer</h3>
+      </div>
+      <div className={style.section}>
+        <h2 className={style.projectH2}>About</h2>
         <p>
           I find myself most days thinking of app ideas, perusing various
           documentation or coding my projects. My weapons of choice are the MERN
           stack, PostgreSQL and a host of cool libraries.
         </p>
+        <p>
+          While I spend most of time coding I also enjoy playing guitar and
+          watching cooking shows.
+        </p>
       </div>
-      <h2 className={style.projectH2}>PROJECTS</h2>
+      <div className={style.section}>
+        <h2 className={style.projectH2}>Skills</h2>
+        <div className={style.skills}>
+          <img src={JS} alt="js" />
+          <img src={Reactpng} alt="js" />
+          <img src={Node} alt="js" />
+          <img src={Express} alt="js" />
+          <img src={Figma} alt="js" />
+          <img src={Sass} alt="js" />
+          <img src={Postgresql} alt="js" />
+          <img src={Redux} alt="js" />
+        </div>
+      </div>
+      <div className={style.section}>
+        <h2 className={style.projectH2}>Projects</h2>
+      </div>
+
       <div className={style.projects}>
         <Project
           site={true}
@@ -75,6 +106,15 @@ const App = () => {
           siteURL={"http://thisisdevmountain.com"}
           githubURL={"https://github.com/C-E-Campbell/groupProjectWPX15"}
         />
+      </div>
+      <div className={style.section}>
+        <h2 className={style.projectH2}>Contact</h2>
+        <h2>
+          <span>Email:</span>
+          <a href="mailto:charles.e.campbell1989@gmail.com">
+            charles.e.campbell1989@gmail.com
+          </a>
+        </h2>
       </div>
     </div>
   );
